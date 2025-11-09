961afa40c9a5689f81211c42df197d83775d656a
Ebisa Getachew <ebisagetachew1243@gmail.com>
2025-11-09 12:52:47 +0300

    added whyAboutUs section and updated some things

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const welcomeContent = document.querySelectorAll(".welcome-content-1");
 const welcomeImageContainer = document.querySelector(
   ".welcome-image-container"
 );
+const whyChooseUsItem = document.querySelectorAll(".why-choose-us-item");
+const whyChooseUsHeading = document.querySelector(".why-choose-us-heading");
+const whyChooseUsTag = document.querySelector(".why-choose-us-tag");
 
 /* Humberger menu */
 
@@ -44,11 +47,11 @@ const welcomeObserver = new IntersectionObserver(
         welcomeContent.forEach((card, index) => {
           setTimeout(() => {
             card.classList.add("visible");
-          }, 500 + index * 400);
+          }, 500 + index * 200);
         });
         setTimeout(() => {
           welcomeImageContainer.classList.add("visible");
-        }, 700);
+        }, 1000);
         welcomeObserver.unobserve(entry.target);
       }
     });
@@ -57,3 +60,27 @@ const welcomeObserver = new IntersectionObserver(
 );
 
 welcomeObserver.observe(document.querySelector(".welcome"));
+
+/* js animation for why choose us */
+
+const whyChooseUsObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        whyChooseUsTag.classList.add("visible");
+        setTimeout(() => {
+          whyChooseUsHeading.classList.add("visible");
+        }, 200);
+        whyChooseUsItem.forEach((card, index) => {
+          setTimeout(() => {
+            card.classList.add("visible");
+          }, 500 + index * 400);
+        });
+        whyChooseUsObserver.unobserve(entries);
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+whyChooseUsObserver.observe(document.querySelector(".why-choose-us"));
